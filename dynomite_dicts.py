d204e34801efb2835ce60c52beca10007428b543
hannahtlz1 <htellez1@go.pasadena.edu>
2021-04-05 12:16:16 -0700

    Create dynomite_dicts.py

---
+++ dynomite_dicts.py
@@ -0,0 +1,11 @@
+pokedex = {}
+
+pokedex["venosaur"] = 'Grass', 'Poisen'
+pokedex['Charizard'] = 'Fire', 'Grass'
+pokedex['Blastoise'] = 'Water'
+
+print(pokedex)
+
+del pokedex['Blastoise']
+
+print(pokedex)
